b8d8418063aa3d1bae5674bddfdd075f24f0e11b
GustavoMeza <coollive11@gmail.com>
2019-03-12 14:46:16 -0600

    Create convex-hull-dp.cpp

---
+++ convex-hull-dp.cpp
@@ -0,0 +1,29 @@
+typedef pair<long long, long long> ii;
+
+const int MAXN = 1e5 + 5;
+const long double EPS = 1e-6;
+
+ii ch[MAXN];
+int M, p;
+
+bool eat(ii l3, ii l2, ii l1) {
+    return ((long double) (l1.second - l3.second)) * (l3.first - l2.first) < ((long double) (l2.second - l3.second)) * (l3.first - l1.first);
+}
+
+long long eval(long long x, ii l) {
+    return l.first * x + l.second;
+}
+
+long long ch_query(long long x) {
+    while(p < M - 1 && eval(x, ch[p]) > eval(x, ch[p + 1]))
+        p++;
+    return eval(x, ch[p]);
+}
+
+void ch_add(long long m, long long b) {
+    ii l = ii(m, b);
+    while(M >= 2 && eat(ch[M - 2], ch[M - 1], l))
+        M--;
+    ch[M++] = l;
+    if(p >= M) p = M - 1;
+}
