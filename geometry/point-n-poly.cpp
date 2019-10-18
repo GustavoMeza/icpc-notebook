5633d552ea6ef20801207610ebb14ac4656901c2
GustavoMeza <coollive11@gmail.com>
2019-10-18 14:01:47 -0500

    Point n poly

---
+++ geometry/point-n-poly.cpp
@@ -0,0 +1,26 @@
+// dos segmentos (a,b), (u,v) se intersectan?
+bool segment_intersect(point a, point b, point u, point v) {
+    auto dir_u = turn(u-a, b-a);
+    auto dir_v = turn(v-a, b-a);
+    auto dir_a = turn(a-u, v-u);
+    auto dir_b = turn(b-u, v-u);
+    return dir_u != dir_v && dir_u != COL && dir_v != COL
+        && dir_a != dir_b && dir_a != COL && dir_b != COL;
+}
+
+// el punto a está dentro o fuera de p?
+bool point_n_poly(poly p, point a) {
+    auto max_x = max_element(begin(p), end(p), [](point a, point b) {
+        return a.x < b.x;
+    })->x;
+    point b(max_x, a.y);
+    int j = size(p)-1;
+    bool ans = false;
+    for(int i = 0; i < size(p); i++) {
+        if(segment_intersect(a, b, p[i], p[j])) {
+            b=!b;
+        }
+        j = i;
+    }
+    return ans;
+}
