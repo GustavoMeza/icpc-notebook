5e14911aa6668d0e3d002b5f23af7f3b621b6843
GustavoMeza <coollive11@gmail.com>
2020-01-21 18:17:07 -0600

    Update binary-lifting-lca.cpp

--- binary-lifting-lca.cpp
+++ binary-lifting-lca.cpp
@@ -1,29 +1,15 @@
-const int MAXN = 2e5+5;
-const int L = 20;
+const ll maxn=2e5+5, L=20;
+vector<ll> ady[maxn];
 
-vector<vi> ady;
-int L, D[MAXN], P[L][MAXN];
-
-int query_binlift(int u, int v) {
-    if(D[u] > D[v]) swap(u, v);
-    int diff = D[v]-D[u];
-    forn(i, L)
-        if(diff & (1<<i))
-            v = P[i][v];
-    for(int i = L-1; i>=0; i--)
-        if(P[i][u] != P[i][v])
-            u = P[i][u], v = P[i][v];
+ll P[L][maxn], D[maxn];
+ll query_binlift(ll u, ll v) {
+    if(D[u] > D[v]) swap(u,v);
+    for(ll i=L-1; i>=0; i--) if(D[u] <= D[P[i][v]]) v=P[i][v];
+    for(ll i=L-1; i>=0; i--) if(P[i][u] != P[i][v]) u=P[i][u], v=P[i][v];
     return u == v ? u : P[0][u];
 }
-
-void build_binlift() {
-    queue<int> q; q.push(0);
-    while(!q.empty()) {
-        int n = q.front(); q.pop();
-        for(int i = 1; i < L; i++)
-            P[i][n] = P[i-1][P[i-1][n]];
-        for(int e : ady[n])
-            if(e != P[0][n])
-                q.push(e), P[0][e] = n, D[e] = D[n]+1;
-    }
+void build_binlift(ll x) {
+    bfs({x});
+    rep(i,1,n+1) P[0][i]=p[i], D[i]=d[i];
+    rep(i,1,L) rep(j,1,n+1) P[i][j]=P[i-1][P[i-1][j]];
 }
