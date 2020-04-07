a4ed5a47f4633f62b21986e50de8617183f2e96c
GustavoMeza <coollive11@gmail.com>
2020-04-07 13:40:27 -0500

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,49 +1,16 @@
 struct mat {
-    int r, c;
-    ll val[MAXN][MAXN];
-
-    ll* operator[] (int i) {
-        return val[i];
-    }
-};
-
-void mat_zeros(mat &m, int r, int c) {
-    memset(&m.r, 0, sizeof(mat));
-    m.r = r; m.c = c;
-}
-
-void mat_copy(mat& a, mat& b) {
-    memcpy(&a.r, &b.r, sizeof(mat));
-}
-
-void mat_eye(mat &m, int n){
-    mat_zeros(m, n, n);
-    for (int i = 0; i < n; i++)
-        m[i][i] = 1;
-}
-
+    ll val[maxn][maxn];
+    mat(){ memset(val, 0, sizeof(val)); }
+    ll* operator[] (ll i) {return val[i];}
+} ady;
 void mat_mul(mat &a, mat &b) {
     mat c;
-    mat_zeros(c, a.r, b.c);
-
-    for (int i = 0; i < c.r; i++)
-        for (int k = 0; k < c.c; k++) {
-            ll v = 0;
-            for (int j = 0; j < a.c; j++)
-                v = (v + (a[i][j] * b[j][k]) % MOD) % MOD;
-            c[i][k] = v;
-        }
-
-    mat_copy(a, c);
+    rep(i,0,n) rep(j,0,n) rep(k,0,n)
+        c[i][j]=(c[i][j]+a[i][k]*b[k][j])%mod;
+    memcpy(&a,&c,sizeof a);
 }
-
-void mat_exp(mat &m, ll n) {
-    mat temp;
-    mat_copy(temp, m);
-    mat_eye(m, m.r);
-    for(; n; n>>=1) {
-        if(n&1) mat_mul(m, temp);
-        mat_mul(temp, temp);
-    }
+void mat_exp(mat &a, ll x) {
+    mat b; rep(i,0,n) b[i][i]=1;
+    for(;x;x/=2,mat_mul(a,a)) if(x&1) mat_mul(b,a);
+    memcpy(&a,&b,sizeof a);
 }
-
