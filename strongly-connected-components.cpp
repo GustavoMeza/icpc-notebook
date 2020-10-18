1b5e25b2e7b41a405ca272053cd27628e7708154
kishtarn555 <hec_montemoon@hotmail.com>
2020-10-17 19:08:12 -0500

    Update strongly-connected-components.cpp

--- strongly-connected-components.cpp
+++ strongly-connected-components.cpp
@@ -1,3 +1,6 @@
+#include<vector>
+#include<stack>
+using namespace std;
 void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, stack<int> & _st, int *lo, int *hi,bool * inStack,bool * vis, int &dt) {
     lo[cur]=hi[cur]=dt++;
     _st.push(cur);
@@ -8,15 +11,15 @@ void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, st
             if (inStack[ni])
                 hi[cur]=min(hi[cur], lo[ni]);
         } else {
-            tarjan(ni, cur,_scc,_adj,st,lo,hi,inStack,vis, dt);
+            tarjan(ni, cur,_scc,_adj,_st,lo,hi,inStack,vis, dt);
             hi[cur]=min(hi[cur], hi[ni]);
         }
     }
     if (lo[cur]==hi[cur]) {
     vector<int> next_cc;
-        while (st.empty()==false && _st.top()!=cur) {
+        while (_st.empty()==false && _st.top()!=cur) {
             next_cc.push_back(_st.top());
-            inStack[st.top()]=false;
+            inStack[_st.top()]=false;
             _st.pop();
         }
         inStack[cur]=false;
@@ -27,17 +30,24 @@ void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, st
 }
 //Recibe N , lista de adyacencia, y un output por referencia, devuelve un vector con las SCC.
 void scc(int N, vector<int> *adj,vector<vector<int> > & output) {
-    
+
     stack<int> st;
     int * lo = new int[N];
     int * hi = new int[N];
-    bool* vis= new int[N];
-    bool* inStack= new int[N];
-    int cont=0;    
+    bool* vis= new bool[N];
+    bool* inStack= new bool[N];
+    int cont=0;
     for (int i =0; i< N; i++) inStack[i]=vis[i]=false;
     for (int i =0; i <N; i++) {
      if (!vis[i]){
-        tarjan(i,i,out, adj, st, lo,hi,inStack, vis, cont);
-     }         
+        tarjan(i,i,output, adj, st, lo,hi,inStack, vis, cont);
+     }
     }
+    delete[] lo;
+    delete[] hi;
+    delete[] vis;
+    delete[] inStack;
+}
+int main() {
+    return 0;
 }
