f1473b35f4a31b023b8de45bd9b2c12598ec3cb8
kishtarn555 <hec_montemoon@hotmail.com>
2020-10-17 19:01:42 -0500

    Update strongly-connected-components.cpp
    No se si me gusta andar pasando todo por referencias... Creo que conviene meter todo en un struct y crear variables allí.

--- strongly-connected-components.cpp
+++ strongly-connected-components.cpp
@@ -1,30 +1,43 @@
-//Tarjan
-int tscc, nscc;
-stack<int> s;
-vi SCC, dfs_num, dfs_low;
-
-void scc(int u) {
-    dfs_low[u] = dfs_num[u] = ++tscc;
-    s.push(u);
-    for(int v : ady[u]) {
-        if(!dfs_num[v]) scc(v);
-        dfs_low[u] = min(dfs_low[u], dfs_low[v]);
+void tarjan(int cur, int prev, vector<vector<int> > &_scc,vector<int> * _adj, stack<int> & _st, int *lo, int *hi,bool * inStack,bool * vis, int &dt) {
+    lo[cur]=hi[cur]=dt++;
+    _st.push(cur);
+    vis[cur]=true;
+    inStack[cur]=true;
+    for (int ni:_adj[cur]) {
+        if (vis[ni]) {
+            if (inStack[ni])
+                hi[cur]=min(hi[cur], lo[ni]);
+        } else {
+            tarjan(ni, cur,_scc,_adj,st,lo,hi,inStack,vis, dt);
+            hi[cur]=min(hi[cur], hi[ni]);
+        }
     }
-
-    if(dfs_low[u] == dfs_num[u]) {
-        int v; nscc++;
-        do {
-            v = s.top(); s.pop();
-            SCC[v] = nscc;
-        } while(u != v);
+    if (lo[cur]==hi[cur]) {
+    vector<int> next_cc;
+        while (st.empty()==false && _st.top()!=cur) {
+            next_cc.push_back(_st.top());
+            inStack[st.top()]=false;
+            _st.pop();
+        }
+        inStack[cur]=false;
+        _st.pop();
+        next_cc.push_back(cur);
+        _scc.push_back(next_cc);
     }
 }
-
-void scc_init() {
-    tscc = nscc = 0;
-    int n = ady.size();
-    SCC.assign(n, 0);
-    dfs_num.assign(n, 0);
-    dfs_low.assign(n, 0);
+//Recibe N , lista de adyacencia, y un output por referencia, devuelve un vector con las SCC.
+void scc(int N, vector<int> *adj,vector<vector<int> > & output) {
+    
+    stack<int> st;
+    int * lo = new int[N];
+    int * hi = new int[N];
+    bool* vis= new int[N];
+    bool* inStack= new int[N];
+    int cont=0;    
+    for (int i =0; i< N; i++) inStack[i]=vis[i]=false;
+    for (int i =0; i <N; i++) {
+     if (!vis[i]){
+        tarjan(i,i,out, adj, st, lo,hi,inStack, vis, cont);
+     }         
+    }
 }
-
