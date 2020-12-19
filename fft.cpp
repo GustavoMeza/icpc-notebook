b34f772cdea8b73205f0f42273fddb417aa4dffa
GustavoMeza <coollive11@gmail.com>
2020-12-19 17:26:18 -0600

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -1,10 +1,4 @@
-const ll maxn=524288;
-ll garner(ll *a, ll *m, ll n, ll mod){
-    rep(i,0,n) rep(j,0,i) a[i]=(a[i]-a[j]%m[i]+m[i])*inv(m[j],m[i])%m[i];
-    ll ans=a[n-1];
-    per(i,0,n-1) ans=(a[i]+ans*m[i])%mod;
-    return ans;
-}
+const ll maxn=(1<<21)+5;
 void ntt(ll *a,ll n,ll g,ll mod){
     for(ll i=0,j=0;i<n;i++) {
         if(i<j) swap(a[i],a[j]);
@@ -12,12 +6,12 @@ void ntt(ll *a,ll n,ll g,ll mod){
         for(;j&bit;bit>>=1) j^=bit;
         j^=bit;
     }
-    for(ll len=2;len<=n;len<<=1){
-        ll wlen=binpow(g,(mod-1)/len,mod);
-        for(ll bat=0;bat+len<=n;bat+=len){
-            for(ll i=bat,wi=1;i<bat+len/2;i++,wi=wi*wlen%mod){
-                ll u=a[i],v=wi*a[i+len/2]%mod;
-                a[i]=(u+v)%mod, a[i+len/2]=(u-v+mod)%mod;
+    for(ll len=1;2*len<=n;len*=2){
+        ll wlen=binpow(g,(mod-1)/(2*len),mod);
+        for(ll i=0;i+2*len<=n;i+=2*len){
+            for(ll j=0,wi=1;j<len;j++,wi=wi*wlen%mod){
+                ll u=a[i|j],v=wi*a[i|len|j]%mod;
+                a[i|j]=u+v<mod?u+v:u+v-mod, a[i|len|j]=u-v<0?u-v+mod:u-v;
             }
         }
     }
@@ -29,14 +23,20 @@ void conv(ll *a, ll *b, ll n, ll g, ll mod){
     ntt(a,n,inv(g,mod),mod);
     rep(i,0,n) a[i]=a[i]*inv(n,mod)%mod;
 }
+ll garner(ll *a, ll *m, ll (&inv)[3][3], ll mod){
+    rep(i,0,3) rep(j,0,i) a[i]=(a[i]-a[j]+m[i])*inv[j][i]%m[i];
+    per(i,0,2) a[i]=(a[i]+a[i+1]*m[i])%mod;
+    return a[0];
+}
 void conv_mod(ll *a, ll *b, ll an, ll bn, ll mod){
-    static ll x[maxn*4], y[maxn*4], ans[maxn*4][3];
-    static ll m[]={(119<<23)+1,(5<<25)+1,(7<<26)+1};
+    static ll x[maxn], y[maxn], ans[maxn][3],minv[3][3];
+    static ll m[]={(5<<25)+1,(7<<26)+1,(119<<23)+1};
+    rep(i,0,3) rep(j,0,3) minv[i][j]=inv(m[i],m[j]);
     ll n=1ll<<64-__builtin_clzll(an+bn-1);
     rep(i,0,3) {
         copy(a,a+n,x), copy(b,b+n,y);
         conv(x,y,n,3,m[i]);
         rep(j,0,n) ans[j][i]=x[j];
     }
-    rep(i,0,n) a[i]=garner(ans[i],m,3,mod);
+    rep(i,0,n) a[i]=garner(ans[i],m,minv,mod);
 }
