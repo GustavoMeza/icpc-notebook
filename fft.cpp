71bd9a5b1a5d5889292ecf993ec2f1001c8cac69
GustavoMeza <coollive11@gmail.com>
2020-12-19 11:00:41 -0600

    Update fft.cpp

--- fft.cpp
+++ fft.cpp
@@ -1,32 +1,42 @@
-const ll MOD = (5<<25) + 1;
-const int MAXM = (1<<21) + 5;
-
-void FFT(ll *P, int n, ll w) {
-    if(n == 1) return;
-    ll Q[2][n>>1];
-    for(int i = 0; i < n; i++) Q[i&1][i>>1]=P[i];
-    FFT(Q[0], n>>1, (w*w) % MOD);
-    FFT(Q[1], n>>1, (w*w) % MOD);
-    ll wi = 1;
-    for(int i = 0; i < n>>1; i++) {
-        P[i] = (Q[0][i] + wi*Q[1][i]) % MOD;
-        P[i+(n>>1)] = (Q[0][i] - wi*Q[1][i]) % MOD;
-        wi = (wi * w) % MOD;
+const ll maxn=524288;
+ll garner(ll *a, ll *m, ll n, ll mod){
+    rep(i,0,n) rep(j,0,i) a[i]=(a[i]-a[j]%m[i]+m[i])*inv(m[j],m[i])%m[i];
+    ll ans=a[n-1];
+    per(i,0,n-1) ans=(a[i]+ans*m[i])%mod;
+    return ans;
+}
+void ntt(ll *a,ll n,ll g,ll mod){
+    for(ll i=0,j=0;i<n;i++) {
+        if(i<j) swap(a[i],a[j]);
+        ll bit=n>>1;
+        for(;j&bit;bit>>=1) j^=bit;
+        j^=bit;
+    }
+    for(ll len=2;len<=n;len<<=1){
+        ll wlen=binpow(g,(mod-1)/len,mod);
+        for(ll bat=0;bat+len<=n;bat+=len){
+            for(ll i=bat,wi=1;i<bat+len/2;i++,wi=wi*wlen%mod){
+                ll u=a[i],v=wi*a[i+len/2]%mod;
+                a[i]=(u+v)%mod, a[i+len/2]=(u-v+mod)%mod;
+            }
+        }
     }
 }
-
-void poly_mult(ll *A, ll *B, int &M) {
-    while(M != (M&-M)) M += M&-M;
-    M<<=1;
-    ll w = 243, winv=114609789, minv = (1-MOD)/M;
-    for(int i = 1; i*M < (1<<25); i<<=1)
-        w = (w*w) % MOD, winv = (winv * winv) % MOD;
-    FFT(A, M, w);
-    FFT(B, M, w);
-    for(int i = 0; i < M; i++) A[i] = (A[i]*B[i]) % MOD;
-    FFT(A, M, winv);
-    for(int i = 0; i < M; i++) {
-        A[i] = (A[i] * minv) % MOD;
-        if(A[i] < 0) A[i] += MOD;
+void conv(ll *a, ll *b, ll n, ll g, ll mod){
+    ntt(a,n,g,mod);
+    ntt(b,n,g,mod);
+    rep(i,0,n) a[i]=a[i]*b[i]%mod;
+    ntt(a,n,inv(g,mod),mod);
+    rep(i,0,n) a[i]=a[i]*inv(n,mod)%mod;
+}
+void conv_mod(ll *a, ll *b, ll an, ll bn, ll mod){
+    static ll x[maxn*4], y[maxn*4], ans[maxn*4][3];
+    static ll m[]={(119<<23)+1,(5<<25)+1,(7<<26)+1};
+    ll n=1ll<<64-__builtin_clzll(an+bn-1);
+    rep(i,0,3) {
+        copy(a,a+n,x), copy(b,b+n,y);
+        conv(x,y,n,3,m[i]);
+        rep(j,0,n) ans[j][i]=x[j];
     }
+    rep(i,0,n) a[i]=garner(ans[i],m,3,mod);
 }
