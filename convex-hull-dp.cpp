68e12ec7a25cc7ea209a9e7cb2876de68f30a241
GustavoMeza <coollive11@gmail.com>
2021-06-10 15:14:56 -0500

    Update convex-hull-dp.cpp

--- convex-hull-dp.cpp
+++ convex-hull-dp.cpp
@@ -6,7 +6,7 @@ ii ch[MAXN];
 int M, p;
 
 bool eat(ii l3, ii l2, ii l1) {
-    return  ((long double) (l1.second - l3.second)) / (l3.first - l1.first) > ((long double) (l2.second - l3.second)) / (l3.first - l2.first);
+    return  ((long double) (l1.second - l3.second)) / (l3.first - l1.first) < ((long double) (l2.second - l3.second)) / (l3.first - l2.first);
 }
 
 long long eval(long long x, ii l) {
